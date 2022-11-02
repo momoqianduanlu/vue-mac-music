924c48ad2145ac2c72a23fd3644e9bed53e7172d
cgs <215966931@qq.com>
2022-11-02 17:21:27 +0800

    init：安装项目依赖

---
+++ vue-mac-music/src/App.tsx
@@ -0,0 +1,11 @@
+import { defineComponent } from 'vue'
+// import { ElButton } from 'element-plus'
+
+export default defineComponent({
+  name: 'SchemaForm',
+  setup(props, ctx) {
+    return () => {
+      return <div class="common-layout"></div>
+    }
+  },
+})
---
+++ vue-mac-music/src/router.ts
@@ -0,0 +1,9 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+const routes: RouteRecordRaw[] = []
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
+
+export default router
